7af437f47139b0825eec89fe8569b2a9db9f3795
Abhigyan Singh <abhi02gyan@gmail.com>
2023-06-18 23:44:59 +0530

    Update LogisticRegression2.py

--- LogisticRegression2.py
+++ LogisticRegression2.py
@@ -1,38 +1,30 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_validate
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
-credit_data = pd.read_csv('credit_data.csv')
+covid_data = pd.read_csv('covid_data.csv')
 
-features = credit_data[['income', 'age', 'loan']]
-target = credit_data.default
+symptoms = covid_data[['Fever', 'Body Ache', 'Age', 'Runny Nose', 'Breathing Problems', 'Sore Throat', 'Dry Cough', 'Loss Of Taste/Smell', 'Red Eyes/Eye Irritation', 'Headache', 'Speech and Mobility Issue', 'Diarrhoea', 'Asthma', 'Heart Disease', 'Diabetic', 'Hypertension', 'Fatigue', 'Vaccinated']]
+target = covid_data['Covid-19 affected']
 
-# machine learning handles arrays not data frames
-X = np.array(features).reshape(-1, 3)
+X = np.array(symptoms).reshape(-1, 18)
 Y = np.array(target)
 
-# 30% of data set is for testing and 70% of data set is for training
-features_train, features_test, target_train, target_test = train_test_split(features, target, test_size=0.3)
+symptoms_train, symptoms_test, target_train, target_test = train_test_split(symptoms, target, train_size=0.7)
 
 model = LogisticRegression()
-model.fit = model.fit(features_train, target_train)
+model.fit = model.fit(symptoms_train, target_train)
 
-predictions = model.fit.predict(features_test)
+prediction = model.fit.predict(symptoms_test)
 
-confusionMatrix = confusion_matrix(target_test, predictions, labels=model.classes_)
+confusionMatrix = confusion_matrix(target_test, prediction, labels=model.classes_)
 display = ConfusionMatrixDisplay(confusion_matrix=confusionMatrix, display_labels=model.classes_)
 print("Confusion matrix: ")
 display.plot()
 plt.show()
 
-print('Accuracy percentage', accuracy_score(target_test, predictions)*100, "%")
-
-
-# using cross validation to predict a more general accuracy
-prediction = cross_validate(model, X, Y, cv=5)
-print("Cross Validation accuracy ", np.mean(prediction['test_score'])*100, "%")
+print('Accuracy percentage', accuracy_score(target_test, prediction)*100, "%")
